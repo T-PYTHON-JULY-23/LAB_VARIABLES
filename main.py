e398e84d9dc4b9418510b559feaa43fa0331d627
naif <naif.albariqi201@hotmail.com>
2023-07-25 13:38:43 +0300

    solution the assignment

---
+++ main.py
@@ -0,0 +1,18 @@
+
+#Define a variable that has a value of your favourite football team.
+my_fav_team = "Real Madrid"
+print(my_fav_team)
+
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+x:int = 12
+y:float = 1.2
+print(type(x))
+print(type(y))
+
+#Assign One Value to Multiple Variables (choose random names & values).
+num1 = num2 = num3 = 4
+print(num1)
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+var1, var2, var3 = 12, 11 ,10
+print(var1,var2,var3)
