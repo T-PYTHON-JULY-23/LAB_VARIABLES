3398eae50191f5da62c275c6bd419ef8deb1b7ee
nada <nada_abdullah_8@hotmail.com>
2023-07-25 13:52:09 +0300

    saveLap

---
+++ answerLapVaribles.py
@@ -0,0 +1,17 @@
+
+
+
+
+football_team:str= "Alhilal"                     #First  Q
+
+x:int = 8                                        #Second Q
+y:float = 8.5
+print(x,y)
+
+a =b =c = 50                                    #Third  Q 
+print(a,b,c)                                    
+A = B  = C = "ABC"
+print(A,B,C)
+
+num1 , num2 , num3 = 1 , 2 , 3                   #Fourth Q 
+name1 , name2 , name3 = "sara" , "nada" , "fay"
