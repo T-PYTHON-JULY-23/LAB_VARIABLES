1f3aba4ace207da5d25dbf7a9021cf96146a4c47
Basma <129796403+basiix1@users.noreply.github.com>
2023-07-25 13:24:27 +0300

    done lab

---
+++ indx.py
@@ -0,0 +1,15 @@
+#Define a variable that has a value of your favourite football team.
+FootballTeam="CHELSEA"
+print(FootballTeam)
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+X = 8
+Y = 8.1
+print(X , Y )
+#Assign One Value to Multiple Variables (choose random names & values).
+E = "basma" , 2 , 9.0
+print(E)
+#Assign multiple Values to Multiple Variables (choose random names & values).
+T , R , C =  7 ,"Ali" , 10
+print(T)
+print(R)
+print(C)
