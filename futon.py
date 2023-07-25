af36697b7cc174722986d34a34c3d85a61840732
Futon <Futonalqahtani>
2023-07-25 14:37:33 +0300

    Done

---
+++ futon.py
@@ -0,0 +1,12 @@
+fav_team = ("inter miami")
+x = 5
+y = 33.5
+fut1 = fut2 = fut3 = 12
+ahm1 , ahm2 , ahm3 = 23 , 32 , 33
+print (ahm1)
+print (fut2)
+print(x)
+print(fav_team)
+print(y)
+print(ahm2)
+print(ahm3)
