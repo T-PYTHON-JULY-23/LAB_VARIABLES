bcfcc640c976354b45df99eed42750ec71539bf3
Hassan Albukhaytan <hassan.s.albukhaytan@gmail.com>
2023-07-25 13:38:25 +0300

    Hassan Albukhaytan_Lab1

---
+++ lab1.py
@@ -0,0 +1,13 @@
+
+
+favourite_team = "Manchester United"
+x = 99
+y = 8.5
+
+var1 = var2 = var3 = 55
+
+x1, y2, z3 = "hassan", 23, 25.5
+print(favourite_team)
+print(x, y)
+print(var1, var2, var3)
+print(x1, " ", y2, " ", z3)
