bc23ca9faedcf81890a3c7c404e97549070365a2
renad1 <renadxx17@gmail.com>
2023-07-25 13:37:46 +0300

    lab 4

---
+++ main.py
@@ -0,0 +1,13 @@
+# 1-Define a variable that has a value of your favourite football team.
+myFavTeam = 'none'
+print('favourite football team:', myFavTeam)
+# 2-Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 5
+y = 15.5
+print('x=',x,'y=',y)
+# 3-Assign One Value to Multiple Variables (choose random names & values).
+value1 = value2 = value3 = '55xxxxx'
+print('1=',value1,'2=',value2,'3=',value3)
+# Assign multiple Values to Multiple Variables (choose random names & values).
+name , age , isPass = 'Sara','22', False
+print('Name:',name,'Age:',age,'is pass?',isPass)
