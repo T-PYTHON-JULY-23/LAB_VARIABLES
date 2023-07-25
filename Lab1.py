4bc0df76ced6fce688787c327dd3e62462605eca
xluuu <mlikmozini@gmail.com>
2023-07-25 13:24:55 +0300

    Finished The Lab

---
+++ Lab1.py
@@ -0,0 +1,14 @@
+# LAB_VARIABLES
+
+#- Define a variable that has a value of your favourite football team.
+My_Team = "Real Madrid" 
+
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 5
+y = 5.5
+
+#- Assign One Value to Multiple Variables (choose random names & values).
+Var1,Var2,Var3 = "Abdulmalik"
+
+#- Assign multiple Values to Multiple Variables (choose random names & values).
+V1 ,V2 , v3 = "Ali" , "Ahmed" , "Abdullah"
