16c90a7a5f498b994ad3840c48092ac5d4ea5b46
Atheer Abdullah <Atheer98.abdullah19@gmail.com>
2023-07-25 13:38:42 +0300

    finished the labb

---
+++ LabVar.py
@@ -0,0 +1,26 @@
+
+
+football_team = "Al Hilal"
+
+x = 15
+
+y = 6.3
+
+var1 = var2 = var3 = 100
+
+var4 , var5 , var6 = 60 , 13 , 'a'
+
+'''
+print(football_team)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+'''
