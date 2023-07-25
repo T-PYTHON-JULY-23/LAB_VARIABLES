97ef58b218b3536aeef5cfd8f597ae638feca903
Khaled_Alghamdi <khaled.alghamdi0@hotmail.com>
2023-07-25 13:16:45 +0300

    Python Lab

---
+++ Main.py
@@ -0,0 +1,10 @@
+
+
+# 1-Define a variable that has a value of your favourite football team.
+# 2-Define a variable “x“  has an integer value and a variable “y” has a float value.
+# 3-Assign One Value to Multiple Variables (choose random names & values).
+# 4-Assign multiple Values to Multiple Variables (choose random names & values).
+
+team="Alhlal"#1 
+x,y = 1 ,1.1 #2 and 4
+str1 =str2=str3="Random String " #3
